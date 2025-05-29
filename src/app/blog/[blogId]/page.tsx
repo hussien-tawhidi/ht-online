5095e2474177b48bef91f6d577bce236a6ec06f0
hussien tawhidi <hussientawhidi710@gmail.com>
2025-05-29 16:09:06 +0330

    blog

--- src/app/blog/[blogId]/page.tsx
+++ src/app/blog/[blogId]/page.tsx
@@ -1,20 +1,7 @@
-import { BlogDetails } from "@/components/blog/BlogDetails";
-import { BlogType } from "@/components/blog/data";
+import React from 'react'
 
-// 🔁 Replace this with a real DB or fetch function
-const mockBlogs: BlogType[] = {
-  id: "1",
-  title: "عنوان مقاله نمونه",
-  description: "توضیح کوتاه",
-  image: "/sample.jpg",
-  content: "<p>محتوای کامل مقاله به صورت HTML...</p>",
-  author: "نویسنده تستی",
-  publishedAt: "2024-01-15",
-  tags: ["تست", "مقاله", "برنامه‌نویسی"],
-};
-
-export default async function BlogPage({ params }: { params: { id: string } }) {
-
-
-  return <BlogDetails blog={mockBlogs} />;
+export default function page() {
+  return (
+    <div>page</div>
+  )
 }
